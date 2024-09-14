ecbe725c6c14800e6a90acc7a204a921af1eab71
MayankPant <mpant4512@gmail.com>
2024-09-14 23:52:00 +0530

    feat: Added a logoff buttom into the main page.

--- src/Components/AppBar.jsx
+++ src/Components/AppBar.jsx
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import logo from "../assets/resources/logo.svg";
 import "../styles/AppBar.css";
 import NavItem from "./NavItem";
-
-
+import { PowerSettingsNew } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import AccessTokenContext from "../context/AccessTokenContext";
+import { Navigate } from "react-router-dom";
 
 const Logo = ({ logo, title }) => {
   return (
@@ -14,15 +16,23 @@ const Logo = ({ logo, title }) => {
   );
 };
 
-
 const navItemStyles = {
   backgroundColor: "#eee25b",
   color: "#673AB7",
 };
 
 class AppBar extends Component {
-  home() {}
+  
+  static contextType = AccessTokenContext;
+
+  logout = async () => {
+    await this.context.logout();
+  }
+  
   render() {
+    if(!this.context.isAuthenticated){
+      return <Navigate to={"login"} />
+    }
     return (
       <div className="app-bar">
         <Logo logo={logo} title={"Task Manager"} />
@@ -43,6 +53,12 @@ class AppBar extends Component {
             routeTo={"/contact"}
             styles={navItemStyles}
           />
+          <IconButton
+            onClick={() => this.logout()}
+            aria-label="logout"
+          >
+            <PowerSettingsNew sx={{ color: "#eee25b", fontSize: 30, marginLeft: "2px" }} />
+          </IconButton>
         </div>
       </div>
     );
